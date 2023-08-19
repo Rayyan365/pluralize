c46011a2b71ced07def9b68e9fd1392916d0b57f
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 07:57:02 -0700

    pluralize assignment done

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Good Luck 💪🏾
+
+const noun = prompt("cat","mouse","bird","chicken","bird");
+
+
+console.log(noun);
+
+
+if ((noun > 1 )){
+
+  result = noun + "s";
+}else{
+
+  result = noun;
+}
+console.log(result);
